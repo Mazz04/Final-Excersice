37f2d4df73d14a7bfa677dc2676c866a518c6a61
Miguel Angel <angelbetancur.zapata@gmail.com>
2023-06-15 12:36:17 -0500

    Added findByName function

--- demo/src/main/java/Repositories/ProductRepository.java
+++ demo/src/main/java/Repositories/ProductRepository.java
@@ -2,6 +2,11 @@ package Repositories;
 
 import Model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
+    List<Product> findByName(String name);
 }

e0a1b73adc8f8cf5bb4030bc6dde4e0f05d1977a
Miguel Angel <angelbetancur.zapata@gmail.com>
2023-06-15 12:00:48 -0500

    Added model for structure

---
+++ demo/src/main/java/Model/Product.java
@@ -0,0 +1,16 @@
+package Model;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String category;
+    private double price;
+    private String size;
+    private int stock;
+}
